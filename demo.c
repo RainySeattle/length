bee0b6408c16e2d4b66b1e9b90b9c4eac19c99b8
Seattle <joyleo@live.cn>
2013-08-02 23:20:25 +0800

    数据读取
    已完成：把文件所有内容按每行读取到buf中，解析出单位转换规则到结构体数组
    已知问题：atof()函数带来的极细小的精度问题，但在可接受范围�

---
+++ demo.c
@@ -0,0 +1,60 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct
+{
+	char measurement[8];
+	float meter;
+}CONVRULE;
+typedef struct
+{
+	char str[40];
+	int lineNumber;
+}BUF;
+
+int main()
+{
+	FILE *fp;
+	CONVRULE convRules[6];
+	BUF buf[17];
+
+	int i = 0;
+
+	if((fp = fopen("input.txt", "r")) == NULL)
+	{
+		printf("could not open file");
+		return 0;
+	}
+
+	///读取到buf
+	while (!feof(fp))
+	{
+		fgets(buf[i].str, 128, fp);
+		i++;
+		buf[i].lineNumber = i;
+		//printf("%d\t", buf[i].lineNumber);
+		//puts(buf[i-1].str);
+	}
+
+	///解析转换规则
+	int j;
+	char tmp[10];
+	for(j=0; j<6; j++)
+	{
+		sscanf(buf[j].str, "%*s %[^=]", &convRules[j].measurement);
+		sscanf(buf[j].str, "%*[^=]=%[^m]", &tmp);
+		convRules[j].meter = atof(tmp);
+
+		//puts(convRules[j].measurement);
+		//puts(tmp);
+		//printf("\t%f\n",convRules[j].meter);
+	}
+
+
+
+	fclose(fp);
+	return 0;
+
+
+}
