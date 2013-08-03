46c5333641ec7d3823b411fb4b615d0994a72ee5
Seattle <joyleo@live.cn>
2013-08-03 12:08:17 +0800

    修复bug，新增calc()函数可进行部分计算
    修复buf里lineNumber错误，修复长度单位为英文单词复数形式造成的匹配问题；
    增加calc()函数，暂时仅可进行单独单位转换。

--- demo.c
+++ demo.c
@@ -1,6 +1,6 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 typedef struct
 {
@@ -12,6 +12,7 @@ typedef struct
 	char str[40];
 	int lineNumber;
 }BUF;
+float calc(char *str, int lineNum);
 
 int main()
 {
@@ -32,9 +33,9 @@ int main()
 	{
 		fgets(buf[i].str, 128, fp);
 		i++;
-		buf[i].lineNumber = i;
-		//printf("%d\t", buf[i].lineNumber);
-		//puts(buf[i-1].str);
+		buf[i-1].lineNumber = i;
+		printf("%d\t", buf[i-1].lineNumber);
+		puts(buf[i-1].str);
 	}
 
 	///解析转换规则
@@ -43,14 +44,52 @@ int main()
 	for(j=0; j<6; j++)
 	{
 		sscanf(buf[j].str, "%*s %[^=]", &convRules[j].measurement);
+		convRules[j].measurement[3] = '\0';  //截取单位前三个字符，英文单词规避复数问题
 		sscanf(buf[j].str, "%*[^=]=%[^m]", &tmp);
 		convRules[j].meter = atof(tmp);
 
-		//puts(convRules[j].measurement);
+		puts(convRules[j].measurement);
 		//puts(tmp);
-		//printf("\t%f\n",convRules[j].meter);
+		printf("\t%f\n",convRules[j].meter);
 	}
 
+
+
+
+
+	float calc(char *str, int lineNum)
+	{
+		float result;
+		int i;
+		if (lineNum >=8 && lineNum <= 13)
+		{
+			float num;
+			char sNum[10];
+			char meas[10];
+			sscanf(str, "%[^ ]", &sNum);
+			sscanf(str, "%*s %s", &meas);
+			meas[3] = '\0';  //截取单位前三个字符，英文单词规避复数问题
+			num = atof(sNum);
+			for (i=0; i<6; i++)
+			{
+				if (strcmp(convRules[i].measurement, meas) == 0)
+				{
+					puts(convRules[i].measurement); //test
+					result = num * convRules[i].meter;
+				}
+
+			}
+
+			//printf("\n\n%f", num); //test
+			//puts(meas); //test
+		}
+
+
+		return result;
+
+	}
+
+	printf("\n\n\n%.2f m\n\n", calc(buf[7].str, buf[7].lineNumber));
 
 
 	fclose(fp);
@@ -58,3 +97,6 @@ int main()
 
 
 }
+
+
+
